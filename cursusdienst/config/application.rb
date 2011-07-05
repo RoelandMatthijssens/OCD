042188beb784289059a08636e9b41a83ed13b21f
Rik Vanmechelen <rik.vanmechelen@gmail.com>
2011-07-05 14:24:00 +0200

    changed gemfile: rspec railes 2.6.1

--- cursusdienst/config/application.rb
+++ cursusdienst/config/application.rb
@@ -13,12 +13,7 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Cursusdienst
   class Application < Rails::Application
-		if Rails.env.test?
-			initializer :after => :initialize_dependency_mechanism do
-				# Work around initializer in railties/lib/rails/application/bootstrap.rb
-				ActiveSupport::Dependencies.mechanism = :load
-			end
-		end
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -50,5 +45,12 @@ module Cursusdienst
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    if Rails.env.test?
+			initializer :after => :initialize_dependency_mechanism do
+				# Work around initializer in railties/lib/rails/application/bootstrap.rb
+				ActiveSupport::Dependencies.mechanism = :load
+			end
+		end
   end
 end
