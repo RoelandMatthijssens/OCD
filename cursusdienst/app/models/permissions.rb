ee5eb3e063f93f1f7962402e83964ee7e68be4c4
Rik Vanmechelen <rik.vanmechelen@gmail.com>
2011-07-11 12:30:23 +0200

    permissions, before change to permission

--- cursusdienst/app/models/permissions.rb
+++ cursusdienst/app/models/permissions.rb
@@ -1,2 +1,9 @@
 class Permissions < ActiveRecord::Base
+  attr_accessible :name, :level
+  
+  validates :name, :level, :presence => true
+  validates_uniqueness_of :level
+  
+  has_many :user
+  
 end
--- cursusdienst/spec/models/permissions_spec.rb
+++ cursusdienst/spec/models/permissions_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Permissions do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @attr = { :name => "root", :level => 0 }
+  end 
+  
+  it "should create a new instance given valid attributes" do
+    Permissions.create!(@attr)
+  end 
+
+  it "should require a name and level"do
+    no_level_permission = Permissions.new(@attr.merge(:name => "", :level => ""))
+    no_level_permission.should_not be_valid
+  end 
+  
 end
