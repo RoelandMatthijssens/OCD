bb8f6a678f4b1e03e2609f7d37ce096180d6fdee
Rik Vanmechelen <rik.vanmechelen@gmail.com>
2011-07-12 16:42:58 +0200

    small changes to faculties model

--- cursusdienst/app/models/faculty.rb
+++ cursusdienst/app/models/faculty.rb
@@ -1,8 +1,10 @@
 #      t.string :name
 
 class Faculty < ActiveRecord::Base
+  attr_accessible :name, :initials, :institute_id
   validates :name, :presence => true
   validates :initials, :presence => true
+  validates :institute_id, :presence => true
   belongs_to :institute
   default_scope :order => "faculties.name ASC"
   has_many :disciplines
