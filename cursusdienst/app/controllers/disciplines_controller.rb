dcfb802c1f8dc2ae01fcd4cf04464efc774e1283
Roeland Matthijssens <rmatthij@vub.ac.be>
2011-08-10 03:40:18 +0200

    pending test for discipline, will do this tomorow, no failing tests now

--- cursusdienst/app/controllers/disciplines_controller.rb
+++ cursusdienst/app/controllers/disciplines_controller.rb
@@ -1,13 +1,10 @@
 class DisciplinesController < ApplicationController
 
    def index
-		deny_access unless signed_in?
-		if current_user.can?("view_disciplines")
-			@title = "Disciplines"
-			@disciplines = Discipline.paginate(:page => params[:page], :per_page => 10)
-		else
-			redirect_to deny_acces_path
-		end
+		deny_access and return unless signed_in?
+		deny_privileged_access and return unless current_user.can?("view_disciplines")
+		@title = "Disciplines"
+		@disciplines = Discipline.paginate(:page => params[:page], :per_page => 10)
   end
 
   def show
