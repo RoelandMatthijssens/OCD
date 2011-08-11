537b06b48bf6c7e7cc43bf21139279194656369b
Rik Vanmechelen <rik.vanmechelen@gmail.com>
2011-08-11 18:16:43 +0200

    before template change

--- cursusdienst/app/controllers/guilds_controller.rb
+++ cursusdienst/app/controllers/guilds_controller.rb
@@ -17,6 +17,7 @@ class GuildsController < ApplicationController
 		deny_privileged_access and return unless current_user.can?('create_guilds')
     @guild = Guild.new
     #@guild.disciplines << Discipline.find(:first) # if Discipline.count > 0
+    @dis_fac_inst = []
     @submit = "Create new Guild"
   end
 
@@ -29,6 +30,7 @@ class GuildsController < ApplicationController
       flash[:notice] = "Guild succesfully created"
       redirect_to @guild
     else
+      @dis_fac_inst = get_dis_fac_inst_from_par(params[:guild])
       flash[:notice] = "NOT created guild. #{params[:guild]}"
       #      flash[:notice] = "NOT created guild. #{get_disciplines_from_guild(params[:guild])}"
       render :action => 'new'
@@ -39,6 +41,7 @@ class GuildsController < ApplicationController
 		deny_access and return unless signed_in?
 		deny_privileged_access and return unless current_user.can?('edit_guilds')
     @guild = Guild.find(params[:id])
+    @dis_fac_inst = get_dis_fac_inst_from_guild(@guild)
     @submit = "Update Guild"
   end
 
@@ -51,6 +54,7 @@ class GuildsController < ApplicationController
       flash[:notice] = "Guild succesfully updated"
       redirect_to @guild
     else
+      @dis_fac_inst = get_dis_fac_inst_from_par(params[:guild])
       render :action => 'edit'
     end
   end
@@ -67,6 +71,43 @@ class GuildsController < ApplicationController
       ds << d if d.instance_of? Discipline and v["_destroy"] != "1"
     } unless par[:disciplines_attributes].nil?
     return ds
+  end
+
+  def get_disciplines_from_guild par
+    ds = []
+    par[:disciplines_attributes].each_value { |v|
+      unless v["id"].empty?
+        d = Discipline.find(v["id"])
+        ds << d if d.instance_of? Discipline and v["_destroy"] != "1"
+      end
+    } unless par[:disciplines_attributes].nil?
+    return ds
+  end
+  
+  def get_dis_fac_inst_from_par par
+    ds = []
+    fac = []
+    inst = []
+    par[:disciplines_attributes].each_value { |v|
+      if v["id"]
+        ds << v["id"] 
+        fac << v["faculty_id"]
+        inst << v["institute_id"]
+      end
+    } unless par[:disciplines_attributes].nil?
+    return [inst, fac, ds]
+  end
+  
+  def get_dis_fac_inst_from_guild guild
+    ds = []
+    fac = []
+    inst = []
+    guild.disciplines.each{ |d|
+      ds << d.id
+      fac << d.faculty_id
+      inst << d.institute_id
+    }
+    return [inst, fac, ds]
   end
 
 end
