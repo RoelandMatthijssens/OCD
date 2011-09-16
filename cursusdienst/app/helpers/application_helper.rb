55f61dc33d8ca22614f329d6feba8c9940d4354f
Rik Vanmechelen <rik.vanmechelen@gmail.com>
2011-09-16 16:50:33 +0200

    application helper kink to add fields => first --> new

--- cursusdienst/app/helpers/application_helper.rb
+++ cursusdienst/app/helpers/application_helper.rb
@@ -13,15 +13,16 @@ module ApplicationHelper
   end
 
   def link_to_add_fields(name, f, association, new_id = Time.now.to_i)
-    new_object = f.object.class.reflect_on_association(association).klass.first
+    new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
       render(association.to_s.singularize + "_fields", :f => builder)
     end
     link_to_function(name, "add_fields(this, '#{association}', '#{new_id}','#{escape_javascript(fields)}')")
   end
   
+
   def add_fields(f, association, new_id = Time.now.to_i)
-    new_object = f.object.class.reflect_on_association(association).klass.first
+    new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
       render(association.to_s.singularize + "_fields", :f => builder, :lol => "kaka")
 #      render(:partial => association.to_s.singularize + "_fields", :locals => {:f => f, :tag => new_id})
--- cursusdienst/app/models/attachment.rb
+++ cursusdienst/app/models/attachment.rb
@@ -1,3 +1,14 @@
 class Attachment < ActiveRecord::Base
   mount_uploader :item, AttachmentUploader
+  
+  before_save :update_item_attributes
+
+  private
+
+  def update_item_attributes
+    if item.present? && item_changed?
+      self.content_type = item.file.content_type
+      self.file_size = item.file.size
+    end
+  end
 end
