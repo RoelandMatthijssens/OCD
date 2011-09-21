385ceeee18bdd655fe9caef5e1c4a164b78a5953
Roeland Matthijssens <rmatthij@vub.ac.be>
2011-09-21 16:25:03 +0200

    untrack config

--- cursusdienst/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Cursusdienst::Application.config.secret_token = 'ad6ea0b656cb83e4885c37b7abc4ced74e374e74a21b8b93a60f6b0175a0b37c26182170973e9a5a1802c6c8478aa71153410d0ad4ac9852befa5a25cb1bfbbc'
