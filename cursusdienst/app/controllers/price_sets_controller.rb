cebc3ca75a5394c4506f83c832f264ed017bbb33
Roeland Matthijssens <rmatthij@vub.ac.be>
2011-09-23 18:58:46 +0200

    permissions handled

--- cursusdienst/app/controllers/price_sets_controller.rb
+++ cursusdienst/app/controllers/price_sets_controller.rb
@@ -1,10 +1,14 @@
 class PriceSetsController < ApplicationController
   def index
+    deny_access and return unless signed_in?
+    deny_privileged_access and return unless current_user.can?('view_price_sets')
     @title = t(:all_price_sets, :scope => "titles" )
     @price_sets = PriceSet.all
   end
 
   def new
+    deny_access and return unless signed_in?
+    deny_privileged_access and return unless current_user.can?('create_price_sets')
     @title = t(:new_price_set, :scope => "titles" )
     if params[:printer_id]
       @price_set = Printer.find(params[:printer_id]).price_sets.new
@@ -14,6 +18,8 @@ class PriceSetsController < ApplicationController
   end
 
   def create
+    deny_access and return unless signed_in?
+    deny_privileged_access and return unless current_user.can?('create_price_sets')
     @price_set = PriceSet.new(params[:price_set])
     @printer = Printer.find(params[:printer_id])
     @printer.price_sets << @price_set
@@ -27,16 +33,22 @@ class PriceSetsController < ApplicationController
   end
 
   def show
+    deny_access and return unless signed_in?
+    deny_privileged_access and return unless current_user.can?('view_price_sets')
     @price_set = PriceSet.find(params[:id])
   end
 
   def edit
+    deny_access and return unless signed_in?
+    deny_privileged_access and return unless current_user.can?('edit_price_sets')
     @title = t(:edit_price_set, :scope => "titles" )
     @price_set = PriceSet.find(params[:id])
     @submit = t(:update_price_set, :scope => "buttons" )
   end
 
   def update
+    deny_access and return unless signed_in?
+    deny_privileged_access and return unless current_user.can?('edit_price_sets')
     @price_set = PriceSet.find(params[:id])
     if @price_set.update_attributes(params[:price_set])
       flash[:success] = t(:update_price_set_success, :scope => "flash" )
--- cursusdienst/app/controllers/printers_controller.rb
+++ cursusdienst/app/controllers/printers_controller.rb
@@ -1,15 +1,21 @@
 class PrintersController < ApplicationController
   def index
+    deny_access and return unless signed_in?
+    deny_privileged_access and return unless current_user.can?('view_printers')
     @title = t(:all_printers , :scope => "titles" )
     @printers = Printer.all
   end
 
   def new
+    deny_access and return unless signed_in?
+    deny_privileged_access and return unless current_user.can?('create_printers')
     @title = t(:new_printer, :scope => "titles" )
     @printer = Printer.new
   end
 
   def create
+    deny_access and return unless signed_in?
+    deny_privileged_access and return unless current_user.can?('create_printers')
     @printer = Printer.new(params[:printer])
     if @printer.save
       flash[:success] = t(:new_printer_success, :scope => "flash")
@@ -21,16 +27,22 @@ class PrintersController < ApplicationController
   end
 
   def show
+    deny_access and return unless signed_in?
+    deny_privileged_access and return unless current_user.can?('view_printers')
     @printer = Printer.find(params[:id])
   end
 
   def edit
+    deny_access and return unless signed_in?
+    deny_privileged_access and return unless current_user.can?('edit_printers')
     @title = t(:edit_printer, :scope => "titles" )
     @printer = Printer.find(params[:id])
     @submit = t(:update_printer, :scope => "buttons")
   end
 
   def update
+    deny_access and return unless signed_in?
+    deny_privileged_access and return unless current_user.can?('edit_printers')
     @printer = Printer.find(params[:id])
     if @printer.update_attributes(params[:printer])
       flash[:success] = t(:update_printer_success, :scope => "flash")
@@ -41,6 +53,7 @@ class PrintersController < ApplicationController
   end
 
   def destroy
-
+    deny_access and return unless signed_in?
+    deny_privileged_access and return unless current_user.can?('delete_printers')
   end
 end
