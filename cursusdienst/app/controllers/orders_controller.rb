b20bc00c7a33feb5336e8932156c6fe1148ce459
Rik Vanmechelen <rik.vanmechelen@gmail.com>
2011-10-25 12:55:01 +0200

    stock blort

--- cursusdienst/app/controllers/orders_controller.rb
+++ cursusdienst/app/controllers/orders_controller.rb
@@ -104,9 +104,7 @@ class OrdersController < ApplicationController
       stock = Stock.find(:all, :conditions => ['guild_id=? AND material_id=?', item.guild.id, item.material.id])
       if stock.any? && stock.first.amount >= item.amount
         stock = stock.first
-        stock.amount -= item.amount
-        stock.floating += item.amount
-        stock.save!
+        stock.stock_to_floating
       end
     end
     #all stock amounts are edited to be floating for the amount ordered by the user
@@ -179,7 +177,7 @@ class OrdersController < ApplicationController
         material = material_order.material
         unless material.printable
           stock = Stock.find(:first, :conditions => ['material_id = ? and guild_id = ?', material.id, material_order.guild.id])
-          if stock & stock.floating > 0
+          if stock && stock.floating > 0
             stock.get_one_from_floating
           end
         end
